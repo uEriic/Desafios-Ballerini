bf6d892343f90a9c1383b8a620b6dba5cd5ba8dd
uEriic <ueriic0@gmail.com>
2023-04-04 19:31:50 -0400

    initial commit

--- 04-23/script.js
+++ 04-23/script.js
@@ -1,10 +1,10 @@
-let btn = document.getElementById('search');
-btn.addEventListener('click', (txt) => {
-    let input = document.getElementById('texto');
-    txt = input.value
-    if (txt.length > 0) {
-        window.open(`https://www.google.com/search?q=${txt}`, "_blank");
-    } else {
-        alert('Por favor informe algum texto para pesquisa.');
-    }
-})
+let btn = document.getElementById("search");
+btn.addEventListener("click", (txt) => {
+  let input = document.getElementById("texto");
+  txt = input.value;
+  if (txt.length > 0) {
+    window.open(`https://www.google.com/search?q=${txt}`, "_blank");
+  } else {
+    alert("Por favor informe algum texto para pesquisa.");
+  }
+});
